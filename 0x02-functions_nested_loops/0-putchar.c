99fd02b60a72feba95046268deb6ba824695cf03
Ahercode <philip>
2022-03-15 13:00:14 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
+#include "main.h"
 #include <stdio.h>
 #include <holberton.h>
-#include <main.h>
 /**
  * main - Print "Holberton" followed by a new line.
  * Description: You are not allowed to include standard libraries.
