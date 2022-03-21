27973689f242adf212c0e195b2f0de5d2ee823f6
Ahercode <philip>
2022-03-21 02:14:21 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  *  * reset_to_98 - takes a pointer to a parameter and updates its value.
- *   * @n: input integer.
- *    * Return: no return.
- *     */
+ *  * @n: input integer.
+ *  * Return: no return.
+ */
 void reset_to_98(int *n)
 {
 		*n = 98;
