b7fb6860a5e9eb48823e68ddd6e23182aa093ee2
Ahercode <philip>
2022-03-21 09:03:04 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,22 @@
 #include "main.h"
+
 /**
- * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
+ * print_rev - print reverse characters.
+ * @s: validate the character
+ * Return: Always 0.
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i = 0;
 
-	while (s[i++])
-		len++;
-
-	for (i = len - 1; i >= 0; i--)
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	for (; i >= 0; i--)
+	{
 		_putchar(s[i]);
-
+	}
 	_putchar('\n');
 }
