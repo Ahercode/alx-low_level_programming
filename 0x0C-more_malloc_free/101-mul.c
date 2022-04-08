da5957619f60b2197656bee61b43866e0b57fa4c
Ahercode <philip>
2022-04-08 06:17:20 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,166 +1,151 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * _atoi_digit - convert a char to integer.
+ * @x: character to convert.
+ * Return: integer.
+ **/
+
+int _atoi_digit(char x)
 {
-	return (write(1, &c, 1));
+	unsigned int res;
+
+	if (x <= '9' && x >= '0')
+		res = x - '0';
+	return (res);
 }
 
 /**
- * create_array - create an array of size size, initialized with a char c
- * @size: size of the array
- * @c: character array is initialized with
- *
- * Return: a pointer to the array, or NULL is failure
- */
-char *create_array(unsigned int size, char c)
-{
-	char *array = NULL;
-	unsigned int i;
-	char *array_cpy;
-
-	if (size == 0)
-		return (NULL);
-
-	array = malloc(sizeof(char) * size);
-	if (array == NULL)
-		return (NULL);
-	array_cpy = array;
-
-	for (i = 0; i < size; i++)
-	{
-		*array_cpy = c;
-		array_cpy++;
-	}
-
-	return (array);
-}
-
-/**
- * mult - multiplies two number strings
- * @prd: where the product is stored
- * @tmp: used for calculating product
- * @len1: length of first factor
- * @len2: length of second factor
- * @av: command line input
- *
- * Return: pointer to character array containing product
- */
-char *mult(char *prd, char *tmp, int len1, int len2, char **av)
-{
-	int i, j, k, l = 0;
-	int digit, d1, d2, carry = 0;
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		k = len1 + len2 - 1;
-		for (j = len2 - 1; j >= -1; j--)
-		{
-			d1 = av[1][i] - '0';
-			d2 = av[2][j] - '0';
-			if (j >= 0)
-				digit = d1 * d2 + carry;
-			else
-				digit = carry;
-			if (digit > 9)
-			{
-				carry = digit / 10;
-				digit = digit % 10;
-			}
-			else
-				carry = 0;
-			tmp[k] = digit + '0';
-			k--;
-		}
-		for (j = len2 + len1 - 1; j >= 0; j--)
-		{
-			digit = tmp[j] - '0' + prd[j - l] - '0' + carry;
-			if (digit > 9)
-			{
-				carry = digit / 10;
-				digit = digit % 10;
-			}
-			else
-				carry = 0;
-			prd[j - l] = digit + '0';
-		}
-		l++;
-	}
-
-	return (prd);
-}
-
-/**
- * print_prod - prints the product of multiplying
- * @prod: array where product is stored
- * @len1: length of first factor
- * @len2: length of second factor
- */
-void print_prod(char *prod, int len1, int len2)
+ * _isNumber - Define if a string is a number.
+ * @argv: Pointer to string.
+ * Return: success (0).
+ **/
+int _isNumber(char *argv)
 {
 	int i;
 
-	for (i = 0; prod[i] == '0'; i++)
-		;
-	for (; i < len1 + len2; i++)
+	for (i = 0; argv[i]; i++)
+		if (argv[i] < 48 || argv[i] > 57)
+			return (1);
+	return (0);
+}
+
+/**
+ *_calloc - allocate array of size * nmemb.
+ * @nmemb: number of elements.
+ * @size: size of element.
+ * Return: pointer to array.
+ **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *tab;
+	unsigned int i;
+
+	tab = malloc(size * nmemb);
+
+	if (tab == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		tab[i] = '0';
+
+	return (tab);
+}
+
+/**
+ * mul_array - multiply two arrays.
+ * @a1: first array.
+ * @len1: length of array a1.
+ * @a2:  char.
+ * @a3: array for result.
+ * @lena: length of array a3.
+ * Return: pointer to array.
+ **/
+
+void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
+{
+	int mul = 0, i, k;
+
+	k = lena;
+	for (i = len1 - 1; i >= 0 ; i--)
 	{
-		_putchar(prod[i]);
+		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
+		a3[k] = (mul % 10) + '0';
+		mul /= 10;
+		k--;
+	}
+
+		while (mul != 0)
+		{
+			mul += a3[k] - '0';
+			a3[k] = (mul % 10) + '0';
+			mul /= 10;
+			k--;
+		}
+
+	return (a3);
+}
+/**
+ * print_array - print all digits of array.
+ * @nb: number of elements to print.
+ * @a: array of elements.
+ **/
+void print_array(char *a, int nb)
+{
+	int i = 0;
+
+	while (a[i] == '0' && (i + 1) < nb)
+	{
+		i++;
+	}
+	for (; i < nb; i++)
+	{
+		_putchar(a[i]);
 	}
 	_putchar('\n');
 }
 
 /**
- * main - multiplies two number strings from input
- * @ac: number of elements in av
- * @av: inputs from command line, numbs to be multiplied
- *
- * Return: 0 (Always Success)
+ *main - print the multiplication of 2 numbers.
+ *@argc: array length.
+ *@argv: array.
+ *Return: 0.
  */
-int main(int ac, char **av)
+
+int main(int argc, char *argv[])
 {
-	int len1, len2;
-	char *temp = NULL; char *prod = NULL;
+	int i, c, len1, len2, lenres;
+	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
+	char *tabres;
 
-	if (ac != 3)
+	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
-		_putchar('E'); _putchar('r'); _putchar('r'); _putchar('o');
-		_putchar('r'); _putchar('\n'); exit(98);
+		for (i = 0; i < 6; i++)
+		{
+			_putchar(E[i]);
+		}
+		exit(98);
 	}
-	for (len1 = 0; av[1][len1] != '\0'; len1++)
-		if (av[1][len1] < '0' || av[1][len1] > '9')
-		{
-			_putchar('E'); _putchar('r'); _putchar('r');
-			_putchar('o'); _putchar('r'); _putchar('\n'); exit(98);
-		}
-	for (len2 = 0; av[2][len2] != '\0'; len2++)
-		if (av[2][len2] < '0' || av[2][len2] > '9')
-		{
-			_putchar('E'); _putchar('r'); _putchar('r');
-			_putchar('o'); _putchar('r'); _putchar('\n'); exit(98);
-		}
-	temp = create_array(len1 + len2 + 1, '0');
-	if (temp == NULL)
-		exit(98);
-	prod = create_array(len1 + len2 + 1, '0');
-	if (prod == NULL)
-		exit(98);
-
-	prod = mult(prod, temp, len1, len2, av);
-
-	print_prod(prod, len1, len2);
-	free(prod);
-	free(temp);
-
+	for (len1 = 0; argv[1][len1]; len1++)
+	;
+	for (len2 = 0; argv[2][len2]; len2++)
+	;
+	lenres = len1 + len2;
+	tabres = _calloc(lenres, sizeof(int));
+	if (tabres == NULL)
+	{
+		free(tabres);
+		return (0);
+	}
+	for (i = len2 - 1, c = 0; i >= 0; i--)
+	{
+	tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+	c++;
+	}
+	print_array(tabres, lenres);
+	free(tabres);
+	exit(EXIT_SUCCESS);
 	return (0);
 }
