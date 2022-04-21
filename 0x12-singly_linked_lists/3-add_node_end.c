660b1e65311a100ad57ff65dd6f600e43e953635
Ahercode <philip>
2022-04-21 16:50:21 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,26 +1,52 @@
 #include "lists.h"
 
-/**
- * free_list - frees a list_t list.
- * @head: pointer to the list.
- **/
-void free_list(list_t *head)
-{
-	list_t *actual_node;
-	list_t *next_node;
+int _strlen_recursion(char *s);
 
-	if (head)
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *aux = *head;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		actual_node = head;
-		next_node = head->next;
-		while (next_node)
-		{
-			free(actual_node->str);
-			free(actual_node);
-			actual_node = next_node;
-			next_node = next_node->next;
-		}
-		free(actual_node->str);
-		free(actual_node);
+		return (NULL);
 	}
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen_recursion(new->str);
+	new->next = NULL;
+
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+		aux->next = new;
+	}
+	else
+		*head = new;
+
+	return (new);
 }
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}}
